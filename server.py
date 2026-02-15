a6710be2418e63fd5f25384568d18efb6835cd43
fizel003 <fizel003@umn.edu>
2026-02-15 02:24:50 -0600

    Server integration

--- server.py
+++ server.py
@@ -3,44 +3,48 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    action = request.args.get("action", "start")
-    gamestates = {
-        "start": {
-            "character": "Character Name",
-            "quote": "Save my life?",
-            "options": [{
-                "text": "Trigger Minigame",
-                "option": "minigame"
-            },
-            {
-                "text": "Don't Trigger Minigame",
-                "option": "no"
-            }]
-        },
-        "no": {
-            "character": "Character Name",
-            "quote": "Are you sure?",
-            "options": [{
-                "text": "Don't want to",
-                "option": "done"
-            },
-            {
-                "text": "Changed my mind",
-                "option": "minigame"
-            }]
-        },
-        "done": {
-            "character": "Character Name",
-            "quote": "OK, sounds good"
-        }
-    }
-    print(action)
+def homepage():
+    return render_template('homepage.html') 
 
-    return render_template(
-        "main.html",
-        gamestate = gamestates[action]
-    )
+# @app.route("/")
+# def hello_world():
+#     action = request.args.get("action", "start")
+#     gamestates = {
+#         "start": {
+#             "character": "Character Name",
+#             "quote": "Save my life?",
+#             "options": [{
+#                 "text": "Trigger Minigame",
+#                 "option": "minigame"
+#             },
+#             {
+#                 "text": "Don't Trigger Minigame",
+#                 "option": "no"
+#             }]
+#         },
+#         "no": {
+#             "character": "Character Name",
+#             "quote": "Are you sure?",
+#             "options": [{
+#                 "text": "Don't want to",
+#                 "option": "done"
+#             },
+#             {
+#                 "text": "Changed my mind",
+#                 "option": "minigame"
+#             }]
+#         },
+#         "done": {
+#             "character": "Character Name",
+#             "quote": "OK, sounds good"
+#         }
+#     }
+#     print(action)
+
+#     return render_template(
+#         "main.html",
+#         gamestate = gamestates[action]
+#     )
 
 @app.route('/hi', methods=['GET'])
 def hello_world2():
